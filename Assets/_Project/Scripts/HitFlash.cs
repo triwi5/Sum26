d10f2ff1b4b551c53ad38104cef22ec6a857ea79
triwi5 <tristwi5@gmail.com>
2026-06-23 17:09:56 +0200

    Hitflash on enemies

---
+++ Assets/_Project/Scripts/HitFlash.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(Health))]
+public class HitFlash : MonoBehaviour
+{
+    [Header("Flash")] 
+    [SerializeField] private Color flashColor = Color.white;
+    [SerializeField] private float flashDuration = 0.1f;
+
+    [Header("Renderer")] 
+    [SerializeField] private Renderer targetRenderer;
+
+    private Health health;
+    private MaterialPropertyBlock propertyBlock;
+    private Color originalColor;
+    private Color currentColor;
+    private Tween activeFlashTween;
+    
+    private static readonly int BaseColorProperty = Shader.PropertyToID("_BaseColor");
+
+    private void Awake()
+    {
+        health = GetComponent<Health>();
+
+        if (targetRenderer == null)
+        {
+            targetRenderer = GetComponentInChildren<Renderer>();
+        }
+        
+        propertyBlock = new MaterialPropertyBlock();
+
+        originalColor = targetRenderer.sharedMaterial.GetColor(BaseColorProperty);
+        currentColor = originalColor;
+    }
+
+    private void OnEnable()
+    {
+        health.OnDamaged += HandleDamaged;
+    }
+
+    private void OnDisable()
+    {
+        health.OnDamaged -= HandleDamaged;
+        activeFlashTween?.Kill();
+    }
+
+    private void HandleDamaged(float amount, Vector3 hitPoint)
+    {
+        Flash();
+    }
+
+    private void Flash()
+    {
+        activeFlashTween?.Kill();
+        currentColor = flashColor;
+        ApplyColor();
+
+        activeFlashTween = DOTween.To(
+            () => currentColor,
+            value =>
+            {
+                currentColor = value;
+                ApplyColor();
+            },
+            originalColor,
+            flashDuration
+            ).SetEase(Ease.OutQuad);
+    }
+
+    private void ApplyColor()
+    {
+        targetRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetColor(BaseColorProperty, currentColor);
+        targetRenderer.SetPropertyBlock(propertyBlock);
+    }
+
+}
